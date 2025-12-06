e41f59ddf5c512347fbde49f37a6d4c4e10897c3
Prekzursil <a_visalon@yahoo.com>
2025-12-06 08:14:39 +0200

    feat: add whisper.cpp backend normalization

--- packages/media-core/src/media_core/transcribe/backends/__init__.py
+++ packages/media-core/src/media_core/transcribe/backends/__init__.py
@@ -2,9 +2,12 @@
 
 from .faster_whisper import normalize_faster_whisper, transcribe_faster_whisper
 from .openai_whisper import transcribe_openai_file
+from .whisper_cpp import normalize_whisper_cpp, transcribe_whisper_cpp
 
 __all__ = [
     "transcribe_openai_file",
     "transcribe_faster_whisper",
     "normalize_faster_whisper",
+    "transcribe_whisper_cpp",
+    "normalize_whisper_cpp",
 ]
